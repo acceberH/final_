6b563b394e2ab7439056851e797f96df35891949
QiongranHuang <qh2130@nyu.edu>
2023-05-01 14:25:22 -0400

    added files

---
+++ js/style.js
@@ -0,0 +1,8 @@
+var t1 = document.getElementById('t1')
+var t2 = document.getElementById('t2')
+t1.addEventListener('click',function(){
+    window.location.href='./privacy.html'
+})
+t2.addEventListener('click',function(){
+    window.location.href='./convinience.html'
+})
